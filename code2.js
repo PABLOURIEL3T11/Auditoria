df9d96556aa1de9d111df78cb17b56b351552d6b
PABLOURIEL3T11 <102884693+PABLOURIEL3T11@users.noreply.github.com>
2024-10-20 18:37:06 -0600

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,21 @@
+// index.js (JavaScript)
+
+function saludar() {
+    console.log("Hola, mundo!");
+    let nombre = "Juan";
+    console.log(miVariableNoExiste); // Error: Variable no definida
+  }
+  
+  // index.css (CSS)
+  body {
+    font-family: Arial;
+    color: blue;
+    background-color: #FF000; // Error: Valor hexadecimal inválido
+  }
+  
+  .boton {
+    background-color: red;
+    font-size: 20px;
+    border-radius: 10px;
+    text-align: center; // Propiedades de texto no válidas en elementos de bloque
+  }
