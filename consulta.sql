073928851106e3d3ec2e33b2a68764b070eae40c
PABLOURIEL3T11 <102884693+PABLOURIEL3T11@users.noreply.github.com>
2024-11-04 21:51:44 -0600

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -1,12 +1,6 @@
-SELECT *
-FROM usuarios u
-JOIN pedidos p ON u.id = p.usuario_id
-JOIN productos pr ON p.producto_id = pr.id
-WHERE u.nombre LIKE '%juan%'
-  AND u.fecha_registro < '2023-01-01'
-  AND p.fecha_pedido = (
-      SELECT MAX(fecha_pedido)
-      FROM pedidos
-      WHERE usuario_id = u.id
-  )
-ORDER BY u.nombre, u.apellido DESC;
+SELECT name, email, age, created_at
+FROM users
+WHERE name LIKE '%John%'
+  OR email LIKE '%john@example.com%'
+  OR age > 30
+ORDER BY created_at DESC;
